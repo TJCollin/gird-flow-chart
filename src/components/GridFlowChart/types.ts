8488bf574691f0fdacc99cb455a1ba97c9cd4d9c
collin <tjzhangcong@outlook.com>
2021-03-31 21:13:58 +0800

    add props to types

--- src/components/GridFlowChart/types.ts
+++ src/components/GridFlowChart/types.ts
@@ -51,3 +51,38 @@ export interface FlowTextType {
 }
 
 export type Direction = "top" | "left" | "bottom" | "right";
+
+export interface FlowChartProps {
+  /**节点 */
+  nodes: FlowNode[];
+  /**连接线 */
+  lines: Line[];
+  /**连接线悬浮颜色 */
+  hoverLineColor?: string;
+  /**连接线默认样式 */
+  defaultLineStroke?: StrokeData;
+  /**列宽 */
+  colWidth?: number;
+  /**行高 */
+  rowHeight?: number;
+  /**节点宽度 */
+  nodeWidth?: number;
+  /**节点高度 */
+  nodeHeight?: number;
+  /**节点距离网格线左方(x)和上方(y)的距离 */
+  margin?: NodeMargin;
+  /**网格线 */
+  grid?: Grid;
+  /**网格线默认样式 */
+  defaultGridStroke?: StrokeData;
+  /**矩形框 */
+  rects?: RectArea[];
+  /**矩形框默认样式 */
+  defaultRectStroke?: StrokeData;
+  /**文字 */
+  texts?: FlowTextType[];
+  /**默认文字样式 */
+  defaultTextFont?: Object;
+  /**转折点离节点的距离 */
+  dis?: number;
+}
